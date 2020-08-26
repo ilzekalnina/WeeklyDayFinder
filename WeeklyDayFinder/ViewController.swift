a42422a7a92ea20beb5c440fb4f594dc7ed83934
Ilze Kalnina <ilze@skate.lv>
2020-08-26 18:31:36 +0300

    functions written, but with errors

--- WeeklyDayFinder/ViewController.swift
+++ WeeklyDayFinder/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var dayTextField: UITextField!
@@ -23,7 +23,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       // dayTextField.delegate = self
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func handleCalculation(){
+        let calendar = Calendar.current
+        var dateComponent = DateComponents()
+        guard let day = dayTextField.text, let month = monthTextField, let year = yearTextField.text else {
+            return
+            
+        }
+        
+        dateComponent.day = Int(day)
+        dateComponent.month = Int(month)
+        dateComponent.year = Int(year)
+        
+        let date = calendar.date(from: dateComponent)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_EN")
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekday = dateFormatter.string(from: date)
+        
+        let capitalizedWeekday = weekday.capitalized
+        
+        resultLabel.text = weekday
+        
+    }
+    
 
     @IBAction func findWeekDayTapped(_ sender: Any) {
     }
