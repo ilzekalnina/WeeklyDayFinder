ea16a88419a962c7a08465ea47ff39ec9973e0a6
Ilze Kalnina <ilze@skate.lv>
2020-08-28 11:49:58 +0300

    made Appearance and Info viewControllers

---
+++ WeeklyDayFinder/AppearenceViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AppearenceViewController.swift
+//  WeeklyDayFinder
+//
+//  Created by Kisacka on 28/08/2020.
+//  Copyright © 2020 Kisacka. All rights reserved.
+//
+
+import UIKit
+
+class AppearenceViewController: UIViewController {
+
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ WeeklyDayFinder/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  WeeklyDayFinder
+//
+//  Created by Kisacka on 28/08/2020.
+//  Copyright © 2020 Kisacka. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
